d6f8ad0abef5c89cc7db6ab65c6c07b7e5612a5b
Matt Player <mattplayer@mac.com>
2020-04-24 17:09:49 +0100

    added ApolloProvider to App

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,38 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+import React from "react"
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  gql,
+  ApolloProvider,
+} from "@apollo/client"
 
-// You can delete this file if you're not using it
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: "https://graphql.fauna.com/graphql",
+    headers: {
+      authorization: `Bearer fnADqNok9eACC3wh80w5hdNCBXNv-WdBhtiA1lBK`,
+    },
+  }),
+})
+
+client
+  .query({
+    query: gql`
+      {
+        allItems {
+          data {
+            name
+            sku
+          }
+        }
+      }
+    `,
+  })
+  .then(result => console.log(result))
+
+// Wraps every page in a component
+export const wrapPageElement = ({ element, props }) => {
+  return <ApolloProvider client={client}>{element}</ApolloProvider>
+}
