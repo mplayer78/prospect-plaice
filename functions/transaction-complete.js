a435924153960570fd1cd5ac940cd63b822e8eed
Matt Player <mattplayer@mac.com>
2020-04-24 11:06:33 +0100

    added success endpoint function

---
+++ functions/transaction-complete.js
@@ -0,0 +1,31 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
+
+exports.handler = async ({ body, header }) => {
+  try {
+    const stripeEvent = stripe.webhooks.constructEvent(
+      body,
+      Headers["stripe-signature"],
+      process.env.STRIPE_WEBHOOK_SECRET
+    )
+
+    if (stripeEvent.type === "checkout.session.completed") {
+      const eventObject = stripeEvent.data.object
+      const items = eventObject.display_items
+      const shippingDetails = eventObject.shipping
+
+      console.log("eventObject", eventObject)
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ received: true }),
+    }
+  } catch (err) {
+    console.log("Stripe webhook failed with ", err)
+
+    return {
+      statusCode: 400,
+      body: `Webhook Error: ${err.message}`,
+    }
+  }
+}
---
+++ src/js/get-confirmation.js
@@ -0,0 +1,6 @@
+export async function getConfirmation() {
+  const data = await fetch("/.netlify/functions/transaction-complete")
+    .then(res => res.json())
+    .then(data => console.log("data", data))
+    .catch(err => console.error("Error: ", err))
+}
